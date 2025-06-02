2cf539b830abbf588ba85a3868d513cd503583e5
Ircama <Ircama@users.noreply.github.com>
2025-06-02 11:21:13 +0200

    Improve depencency checks during installation

--- pysnmp_sync_adapter/__init__.py
+++ pysnmp_sync_adapter/__init__.py
@@ -1,3 +1,37 @@
+import sys
+import pysnmp
+
+try:
+    # Python 3.8+
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    # For Python <3.8
+    from importlib_metadata import version, PackageNotFoundError
+
+try:
+    pysnmp_version = version("pysnmplib")
+    raise ImportError(
+        f"pysnmp-sync-adapter requires pysnmp>=7.0.0. "
+        f"Detected version: pysnmplib {pysnmp_version}. "
+        f"Please uninstall pysnmplib with `pip uninstall pysnmplib`. "
+        f"Then install the latest pysnmp version with `pip install pysnmp`."
+    )
+except PackageNotFoundError:
+    pass
+
+try:
+    pysnmp_version = version("pysnmp")
+    if pysnmp_version.startswith("4.") or pysnmp_version.startswith("5."):
+        raise ImportError(
+            f"pysnmp-sync-adapter requires pysnmp>=7.0.0. "
+            f"Detected version: {pysnmp_version}. "
+            f"Please uninstall pysnmp with `pip uninstall pysnmp`. "
+            f"Then install the latest pysnmp version with `pip install pysnmp`."
+        )
+except PackageNotFoundError:
+    print("pysnmp not found. Please install pysnmp>=7.0.0.", file=sys.stderr)
+    raise
+
 from .sync_adapters import (
     get_cmd_sync,
     next_cmd_sync,
